774b69d2b72902ca81aaa3a7f7f993a40fffefd6
awalariansyah <awalariansyah7@gmail.com>
2020-12-07 21:12:22 +0700

    add dosen search

--- commands/dosen.js
+++ commands/dosen.js
@@ -1,26 +1,29 @@
 module.exports = {
 	name: 'carikan',
-	description: `Perintah untuk mencari mahasiswa berdasarkan nama/nim/wa/email.\n\` alel carikan <nim>/<namaDepan/belakang/tengah>/<wa>/<email> \``,
+	description: `Perintah untuk mencari dosen berdasarkan nama/wa/email.\n\` alel dosen <matkul>/<namaDepan/belakang/tengah>/<wa>/<email> \``,
 	args: true,
-	execute(message, args, Discord, mhs){
+	execute(message, args, Discord){
+		const dosen = require('/app/data/dosen.json')
+
 		const keyword = args[0].toLowerCase();
-		const mhsObj = mhs.filter( el => Object.entries(el).flat().map( el => el.toLowerCase()).includes(keyword))
+		const dosenObj = dosen.filter( el => Object.entries(el).flat().map( el => el.toLowerCase()).includes(keyword))
 
 		if (mhsObj == 0) return message.reply('Mohon maaf pencarian tidak ditemukan.');
-		mhsObj.forEach( result => {
+		dosenObj.forEach( result => {
 		const words = `${result.namaDepan} ${result.namaTengah} ${result.namaBelakang}`;
 
 		const name = words.trim().split(' ').map( word => word[0].toUpperCase() + word.substring(1)).join(' ')
+		const nameTitle = `${name} ${result.gelar}`
 
 		const profileEmbed = new Discord.MessageEmbed()
 			.setColor('RANDOM')
-			.setTitle(`Kartu Profil`)
-			.addField('Nama', `${name}`)
-			.addField('NIM', `${result.nim}`)
+			.setTitle(`Kartu Profil Dosen`)
+			.addField('Nama', `${nameTitle}`)
+			.addField('Mengampu Mata Kuliah', `${result.matkul}`)
 			.addField('Whatsapp', `${result.whatsapp}`)
 			.addField('Email', `${result.email}`)
 			.setImage(`https://raw.githubusercontent.com/awalariansyah/alel/main/data/${result.img}`)
-			.setFooter(`Kartu ini berlaku hingga 2023 - Alel`)
+			.setFooter(`Mahasiswa harus melampaui dosennya - Alel`)
 
 		message.channel.send(profileEmbed);
 	})
