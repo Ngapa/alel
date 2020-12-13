25499526cf21228f326b9d20d0203ad65f34ea0f
awalariansyah <awalariansyah7@gmail.com>
2020-12-13 07:28:20 +0700

    pausing development of alel

--- commands/pilihMana.js
+++ commands/pilihMana.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'pilihmana',
     description: 'Perintah untuk meminta Alel memilih dari beberapa pilihan.\n` alel pilihmana <opsi1>,<opsi2>,... `',
     execute(message, args, Discord){
-        let choice = args.join('').split(/,+/)
+        let choice = args.join(' ').split(/,+/)
         let myChoice = choice[Math.floor(Math.random()* choice.length)]
         
         return message.channel.send('Alel lebih milih...').then(m => {
