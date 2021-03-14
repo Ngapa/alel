54caf6d5892d71d5f62ce68b7382581c69d4b05d
snowfluke <awalariansyah7@gmail.com>
2021-03-14 13:57:45 +0700

    revert

--- commands/komputama.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'komputama',
-    description: 'Perintah menyapa siswa siswi SMK Komputama Majenang',
-    execute(message){
-        message.reply('Halo, siswa siswi SMK Komputama Majenang!! :wave:')
-    }
-}
