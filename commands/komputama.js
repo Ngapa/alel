6f5e2d2cab89f6cfbec75287cf154e94883b3dd2
snowfluke <awalariansyah7@gmail.com>
2021-03-14 13:53:55 +0700

    perintah baru

---
+++ commands/komputama.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'komputama',
+    description: 'Perintah menyapa siswa siswi SMK Komputama Majenang',
+    execute(message){
+        message.reply('Halo, siswa siswi SMK Komputama Majenang!! :wave:')
+    }
+}
