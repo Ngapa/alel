e32b9288d32a32ece1557d2b521416bcf34e3915
snowfluke <awalariansyah7@gmail.com>
2021-03-14 14:27:10 +0700

    debug levelup message

--- main.js
+++ main.js
@@ -68,6 +68,8 @@ client.on("message", message => {
                 .setColor('#5CE1E6')
                 .setFooter(message.guild.name)
 
+            console.log(message.author.id, levelUpEmbed)
+
             message.channel.send(`<@!${message.author.id}> ${levelUpEmbed}`);
 
         }
